6c048eedcc1a25001fd4c1a9b3c53f70512f6382
Dmitrii-01 <144121834+Dmitrii-01@users.noreply.github.com>
2026-01-03 22:19:25 +0300

    catalog routing

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask, redirect, url_for
-
 from flaskr.db import get_db
 
 def create_app():
@@ -7,11 +6,12 @@ def create_app():
 
     from . import (
         db,
-        home, register
+        home, register, catalog
     )
     db.init_db_in_app(app)
     app.register_blueprint(home.bp)
     app.register_blueprint(register.bp)
+    app.register_blueprint(catalog.bp)
 
     @app.route('/')
     def base():
