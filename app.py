232f92bdf55cf1a157f3f633e946dd1b39ebe47e
Dmitrii-01 <144121834+Dmitrii-01@users.noreply.github.com>
2026-01-07 02:48:52 +0300

    refined files

--- app.py
+++
@@ -1,8 +0,0 @@
-from flaskr import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5003)  # или любой другой порт: 8000, 3000 и т.д.
-
-#http://127.0.0.1:5003
--- flaskr/models/userbook.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class UserBook:
-    user_id: int
-    book_id: int
-    rating: int | None = None      # оценка от 1 до 5 (или None)
-    review: str | None = None      # текст отзыва
