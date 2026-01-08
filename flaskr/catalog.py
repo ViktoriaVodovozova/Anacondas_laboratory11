0c93930c707c28804f42b5868f4419ea1a8069d4
Dmitrii-01 <144121834+Dmitrii-01@users.noreply.github.com>
2026-01-09 02:21:40 +0300

    catalog and profile updated

--- flaskr/catalog.py
+++ flaskr/catalog.py
@@ -1,6 +1,7 @@
 from sqlalchemy import select
-from flask import Blueprint, render_template, current_app, session, Response, url_for
+from flask import Blueprint, render_template, current_app, session, Response, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import SQLAlchemyError
 from werkzeug.utils import redirect
 from flaskr.models.book import Book
 from flaskr.models.user_book import UserBook
@@ -8,23 +9,49 @@ from flaskr.models.user_book import UserBook
 bp = Blueprint('catalog', __name__, url_prefix='/catalog')
 
 @bp.route('/')
-def catalog() -> str:
+def catalog() -> Response | str:
     db: SQLAlchemy = current_app.extensions['db']
-    stmt = select(Book)
-    catalog = db.session.scalars(stmt).all()
-    return render_template('catalog.html', catalog=catalog)
+    try:
+        stmt = select(Book)
+        catalog = db.session.scalars(stmt).all()
+        return render_template('catalog.html', catalog=catalog)
+    except SQLAlchemyError as _:
+        db.session.rollback()
+        flash('Ошибка загрузки каталога', 'error')
+        return redirect(url_for('home.home'))
 
 @bp.route('/<int:book_id>')
-def book(book_id: int) -> str:
+def book(book_id: int) -> Response | str:
     db: SQLAlchemy = current_app.extensions['db']
-    book = db.session.get_one(Book, book_id)
-    return render_template('book.html', book=book)
+    try:
+        book = db.session.get_one(Book, book_id)
+        return render_template('book.html', book=book)
+    except SQLAlchemyError as _:
+        db.session.rollback()
+        flash('Книга не найдена в общем каталоге', 'info')
+        return redirect(url_for('home.home'))
 
 @bp.route('/<int:book_id>/add_book', methods=['POST'])
 def add_book(book_id: int) -> Response:
     db: SQLAlchemy = current_app.extensions['db']
-    user_id = session['user_id']
-    user_book = UserBook(user_id=user_id, book_id=book_id)
-    db.session.add(user_book)
-    db.session.commit()
-    return redirect(url_for('catalog.book', book_id=book_id))
+
+    user_id = session.get('user_id', None)
+    if user_id is None:
+        flash('Войдите в систему для возможности добавления книг в личный каталог', 'info')
+        return redirect(url_for('catalog.book', book_id=book_id))
+
+    try:
+        user_book = db.session.get(UserBook, (user_id, book_id))
+        if user_book is not None:
+            flash('Книга уже находится в личном каталоге', 'info')
+        else:
+            user_book = UserBook(user_id=user_id, book_id=book_id)
+            db.session.add(user_book)
+            db.session.commit()
+            flash('Книга добавлена в личный каталог', 'success')
+        return redirect(url_for('catalog.book', book_id=book_id))
+    except SQLAlchemyError as _:
+        db.session.rollback()
+        flash('Ошибка добавления книги в личный каталог', 'error')
+        return redirect(url_for('catalog.catalog'))
+
--- flaskr/profile.py
+++ flaskr/profile.py
@@ -13,70 +13,76 @@ bp = Blueprint('profile', __name__, url_prefix='/profile')
 def profile(user_id: int) -> Response | str:
     db: SQLAlchemy = current_app.extensions['db']
 
-    session_user_id = session.get('user_id', None)
-    error = False
-    if session_user_id is None:
-        flash('Зарегестрируйтесь, чтобы просматривать профиль')
-        error = True
-    elif user_id != session_user_id:
-        flash('У вас нет прав зелезать в чужую шкуру')
-        error = True
-    if error:
+    if not check_user_access(user_id):
         return redirect(url_for('home.home'))
 
-    user = db.session.get_one(User, user_id)
-    stmt = (
-        select(
-            Book.id, Book.name, Book.author,
-            UserBook.rating, UserBook.review
+    try:
+        user = db.session.get(User, user_id)
+        stmt = (
+            select(
+                Book.id, Book.name, Book.author,
+                UserBook.rating, UserBook.review
+            )
+            .join(UserBook, UserBook.book_id == Book.id)
+            .where(UserBook.user_id == user_id)
         )
-        .join(UserBook, UserBook.book_id == Book.id)
-        .where(UserBook.user_id == user_id)
-    )
-    user_books = db.session.execute(stmt).all()
+        user_books = db.session.execute(stmt).all()
 
-    return render_template('profile.html',
-                           user_id=user_id,
-                           email=user.email,
-                           nickname=user.nickname,
-                           user_books=user_books)
+        return render_template('profile.html',
+                               user_id=user_id,
+                               email=user.email,
+                               nickname=user.nickname,
+                               user_books=user_books)
+    except SQLAlchemyError as _:
+        db.session.rollback()
+        flash('Ошибка загрузки профиля', 'error')
+        return redirect(url_for('home.home'))
 
 @bp.route('/<int:user_id>/update_book/<int:book_id>', methods=['POST'])
 def update_book(user_id: int, book_id: int) -> Response:
     db: SQLAlchemy = current_app.extensions['db']
 
-    session_id = session.get('user_id', None)
-    if session_id is None or user_id != session_id:
+    if not check_user_access(user_id):
+        return redirect(url_for('home.home'))
+
+    try:
+        user_book = db.session.get(UserBook, (user_id, book_id))
+    except SQLAlchemyError as _:
+        db.session.rollback()
+        flash('Ошибка сохранения отзыва', 'error')
         return redirect(url_for('profile.profile', user_id=user_id))
 
-    user_book = db.session.get_one(UserBook, (user_id, book_id))
     if user_book is None:
-        flash('Книга либо не существует, либо не добавлена в профиль', 'info')
+        flash('Книга либо не существует, либо не добавлена в личный каталог', 'info')
         return redirect(url_for('profile.profile', user_id=user_id))
 
     new_rating = request.form.get('rating', None)
     new_review = request.form.get('review', None)
-
     try:
         user_book.rating = new_rating
         user_book.review = new_review
         db.session.commit()
-        flash('Отзыв сохранен', 'success')
+        flash('Отзыв успешно сохранен', 'success')
         return redirect(url_for('profile.profile', user_id=user_id))
     except SQLAlchemyError as _:
         db.session.rollback()
-        flash('Ошибка добавления', 'error')
+        flash('Ошибка сохранения отзыва', 'error')
         return redirect(url_for('profile.profile', user_id=user_id))
 
 @bp.route('/<int:user_id>/delete_book/<int:book_id>', methods=['POST'])
 def delete_book(user_id: int, book_id: int) -> Response:
     db: SQLAlchemy = current_app.extensions['db']
 
-    session_id = session.get('user_id', None)
-    if session_id is None or user_id != session_id:
+    if not check_user_access(user_id):
+        return redirect(url_for('home.home'))
+
+    try:
+        user_book = db.session.get(UserBook, (user_id, book_id))
+    except SQLAlchemyError as _:
+        db.session.rollback()
+        flash('Ошибка удаления книги', 'error')
         return redirect(url_for('profile.profile', user_id=user_id))
 
-    user_book = db.session.get_one(UserBook, (user_id, book_id))
     if user_book is None:
         flash('Книга либо не существует, либо не добавлена в личный каталог', 'info')
         return redirect(url_for('profile.profile', user_id=user_id))
@@ -84,10 +90,18 @@ def delete_book(user_id: int, book_id: int) -> Response:
     try:
         db.session.delete(user_book)
         db.session.commit()
-        flash('Книга удалена из персонального каталога', 'success')
+        flash('Книга успешно удалена из личного каталога', 'success')
         return redirect(url_for('profile.profile', user_id=user_id))
     except SQLAlchemyError as _:
-        flash('Ошибка удаления', 'error')
+        flash('Ошибка удаления книги', 'error')
         return redirect(url_for('profile.profile', user_id=user_id))
 
-
+def check_user_access(user_id: int) -> bool:
+    session_user_id = session.get('user_id', None)
+    if session_user_id is None:
+        flash('Войдите в систему для просмотра профиля', 'error')
+        return False
+    if user_id != session_user_id:
+        flash('У вас нет прав зелезать в чужую шкуру', 'error')
+        return False
+    return True
